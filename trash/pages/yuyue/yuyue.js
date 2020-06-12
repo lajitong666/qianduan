a057fa5e38257634bb73d796c7c958930da53d50
shange <767234318@qq.com>
2020-06-13 00:12:03 +0800

    预约管理差 与后端对接

--- trash/pages/yuyue/yuyue.js
+++ trash/pages/yuyue/yuyue.js
@@ -3,25 +3,22 @@ Page({
   StatusBar: app.globalData.StatusBar,
   CustomBar: app.globalData.CustomBar,
   data: {
-      picker: ['服务人员1', '服务人员2', '服务人员3'],
-      value1: '', //称呼
-      value2: '', //联系电话
-      value3: '', //详细地址
-      person: '',  //人员选择
-      multiIndex: [0, 0, 0],
-      time: '12:01', //时间
-      date: '2018-12-25',  //日期
-      region: ['广东省', '广州市', '海珠区'],
-      imgList: [],
-      modalName: null,
-      textareaAValue: '',
-      textareaBValue: '',
-      index: null,
+    servicePersonList: [],
+    name: '', //称呼
+    phone: '', //联系电话
+    address: '', //详细地址
+    person: '',  //人员选择
+    time: '12:01', //时间
+    date: '2018-12-25',  //日期
+    serve: '',
+    modalName: null,
+    index: null,
+    serviceType: []
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     var that = this;
     wx.request({
-      url: (app.globalData.url+'/servicetable/getAllService'),
+      url: (app.globalData.url + '/servicetable/getAllService'),
       method: 'post',
       data: {
       },
@@ -29,13 +26,19 @@ Page({
         "content-type": "application/x-www-form-urlencoded", // 默认值    
         'csrf-csrf': 'csrf-csrf'
       },
-      success:function(res){
+      success: function (res) {
+        console.log("这里是服务人会员")
         console.log(res.data.data)
-          that.setData({
-            serviceList:res.data.data
-          })
+        var list = [];
+        for (var i = 0; i < res.data.data.length; i++) {
+          list.push(res.data.data[i].name);
+        }
+        console.log(list);
+        that.setData({
+          servicePersonList: list
+        })
       }
-  })
+    })
   },
   DateChange(e) {
     this.setData({
@@ -43,44 +46,38 @@ Page({
     })
   },
 
-  @RequestParam(value = "served") String served,
-  @RequestParam(value = "phone") String phone,
-  @RequestParam(value = "date") String date,
-  @RequestParam(value = "serve") String serve,
-  @RequestParam(value = "address") String address
 
 
-
-  addOne(e){
+  addOne(e) {
     var that = this;
     this.setData({
       modalName: e.currentTarget.dataset.target
     })
     wx.request({
-      url: (app.globalData.url+'/servicetable/addOne'),
+      url: (app.globalData.url + '/servicetable/addOne'),
       method: 'post',
       data: {
-        served:that.value1,
-        phone:that.value2,
-        date:that.date+that.time,
-        serve:123,  //这里还没有搞好。
-        address:that.value3
+        served: that.value1,
+        phone: that.value2,
+        date: that.date + that.time,
+        serve: 123,  //这里还没有搞好。
+        address: that.value3
       },
       header: {
         "content-type": "application/x-www-form-urlencoded", // 默认值    
         'csrf-csrf': 'csrf-csrf'
       },
-      success:function(res){
+      success: function (res) {
         console.log(res.data.data)
-          that.setData({
-            serviceList:res.data.data
-          })
+        that.setData({
+          serviceList: res.data.data
+        })
       }
-  })
-  
+    })
+
 
   },
-  changeInput(e){
+  changeInput(e) {
     let changed = {};
     let prop = e.currentTarget.dataset.prop
     changed[prop] = e.detail.value;
@@ -91,7 +88,7 @@ Page({
       time: e.detail.value
     })
   },
-  PickerChange(e) {
+  PersonChange(e) {
     console.log(e);
     var that = this;
     this.setData({
