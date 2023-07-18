278494a8b85ad9276024e36239056f77c494f8f2
ping ping <51507451+abc767234318@users.noreply.github.com>
2023-07-18 11:52:23 +0800

    7.18

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,126 +1,121 @@
-const app = getApp()
+var app = getApp();
 
 Page({
-  options: {
-    addGlobalClass: true,
-  },
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    starCount: 0,
-    forksCount: 0,
-    visitTotal: 0,
+    ColorList: app.globalData.ColorList,
+    cardCur: 0,
+    scanResult:'',
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=104813898,1898163406&fm=26&gp=0.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2056146150,3067592488&fm=26&gp=0.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2973598070,2257499655&fm=15&gp=0.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=360835900,3233160965&fm=26&gp=0.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300419795,3282529978&fm=26&gp=0.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1888801328,1076456724&fm=11&gp=0.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585802157914&di=b1cd753140277bf68e64d1bd702b1f3a&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20190705%2Fca99b15d6da8440dada3a1da8a3d95d9.gif'
+    }],
+    
   },
   onLoad: function (options) {
     var that = this
-    console.log(app.globalData.openId)
-    console.log(app.globalData.openId)
-    wx.getStorage({//获取本地缓存
-      key: "userinformation",
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          user: res.data
-        })
+    wx.request({
+      url: (app.globalData.url + '/user/getAppSecure'),
+      method: 'post',
+      data: {
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded", // 默认值    
+        'csrf-csrf': 'csrf-csrf'
+      },
+      success: function (response) {
+        app.globalData.appSecret = response.data.msg
       }
-    })
-    wx.getStorage({
-      key: 'openId',
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          openId: res.data
-        }),
-        console.log("正在获取钟")
-        wx.request({
-          url: (app.globalData.url + '/user/getUserDetails'),
-          method: 'post',
-          data: {
-            openId: res.data
-          },
-          header: {
-            "content-type": "application/x-www-form-urlencoded", // 默认值    
-            'csrf-csrf': 'csrf-csrf'
-          },
-          success: function (response) {
-            console.log(response)
-            that.setData({
-              userDetails: response.data.data
-            })
-          }
-        })
-      }
-    })
+    }),
+    wx.login({
+      success(res) {
+          if (res.code) {
+              // 发起网络请求
+              console.log('用户code'+res.code)          
+              console.log(app.globalData.appSecret)        
+              wx.request({
+                  url: ('https://api.weixin.qq.com/sns/jscode2session?appid=wx5e3066fcc86059d3&secret='+app.globalData.appSecret+'&js_code=' + res.code + '&grant_type=authorization_code'),
+                  // '?m=home&c=Api&a=getOpenId&appid=' + app.globalData.appId + '&secret=' + app.globalData.appSecret + '&js_code=' + res.code + '&grant_type=authorization_code').replace(/\s+/g, ""
+                  // 'https://api.weixin.qq.com/sns/jscode2session?appid=' + app.globalData.appId + '&secret=' + app.globalData.appSecret + '&js_code=' + res.code + '&grant_type=authorization_code',
+                  header: {
+                      'content-type': 'application/json' // 默认值                     
+                  },
+                  method: "GET",
+                  success(res) {
+                      app.globalData.openId = res.data.openid;
+                      console.log(res)
+                      console.log("分割线")
+                      wx.setStorage({
+                        key: 'openId',
+                        data: res.data.openid
+                     })
 
+                  }
+              })
+          }
+      }
+    })
+  
+    wx.request({ //获取垃圾与收益
+      url: (app.globalData.url + '/trash/get'),
+      method: 'POST',
+      data: {
+        id:1 
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded",
+        'csrf-csrf': 'csrf-csrf'
+      },
+      success: function (response) {
+        console.log(response.data.data)
+        that.setData({
+          trash:response.data.data[0]['trash'],
+          profit:response.data.data[0]['profit']
+        })
+      }
+    }) 
   },
-
-
-
-
-    attached() {
-      console.log("success")
-      let that = this;
-      wx.showLoading({
-        title: '数据加载中',
-        mask: true,
-      })
-      let i = 0;
-      numDH();
-      function numDH() {
-        if (i < 20) {
-          setTimeout(function () {
-            that.setData({
-              starCount: i,
-              forksCount: i,
-              visitTotal: i
-            })
-            i++
-            numDH();
-          }, 20)
-        } else {
-          that.setData({
-            visitTotal: that.coutNum(10),
-            starCount: that.coutNum(10),
-            forksCount: that.coutNum(200)
-          })
-        }
+  scanCode:function(options){
+    var that = this;
+    wx.scanCode({
+      success (res) {
+        console.log(res)
+        that.data.scanResult = res.result;
+        that.setData({
+          scanResult:res.result
+        })
+      },
+      fail(res){
+        console.log("扫码失败")
       }
-      wx.hideLoading()
-    },
-    methods: {
-      coutNum(e) {
-        if (e > 1000 && e < 10000) {
-          e = (e / 1000).toFixed(1) + 'k'
-        }
-        if (e > 10000) {
-          e = (e / 10000).toFixed(1) + 'W'
-        }
-        return e
-      },
-      CopyLink(e) {
-        wx.setClipboardData({
-          data: e.currentTarget.dataset.link,
-          success: res => {
-            wx.showToast({
-              title: '已复制',
-              duration: 1000,
-            })
-          }
-        })
-      },
-      showModal(e) {
-        this.setData({
-          modalName: e.currentTarget.dataset.target
-        })
-      },
-      hideModal(e) {
-        this.setData({
-          modalName: null
-        })
-      },
-      showQrcode() {
-        wx.previewImage({
-          urls: ['https://image.weilanwl.com/color2.0/zanCode.jpg'],
-          current: 'https://image.weilanwl.com/color2.0/zanCode.jpg' // 当前显示图片的http链接      
-        })
-      },
-    }
-  })
+    })
+  }
+})
